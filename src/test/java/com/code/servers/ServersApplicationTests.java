45c2bfeb6415cf1238fc4a438a8882b1752a4b88
Tapiwanashe Shoshore <tapiwanashe.shoshore@wonderlabz.com>
2022-02-16 19:58:28 +0200

    First commit

---
+++ src/test/java/com/code/servers/ServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.servers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
